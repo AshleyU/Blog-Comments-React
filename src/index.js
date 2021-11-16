839b980a11760ad40f9c7122eab4713bc5360b45
Ashley U <ashley.umetsu@gmail.com>
2021-11-16 15:03:38 -0600

    passing props for all comments

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
+            <CommentDetail author="Sam" time="Today at 6:30PM" comment="Great article!" />
+            <CommentDetail author="Jordan" time="July 5th at 2:32PM" comment="I am going to save this for later. Thanks." />
+            <CommentDetail author="Alex" time="October 28th at 11:58AM" comment="Wow! This really changed my perspective."/>
         </div>
     );
 };
